4c82bc823e2fbbdf438db14a34bdb1dbcf14e13a
Artem Prokop <artem13.prokop@gmail.com>
2023-11-11 18:12:30 +0100

    Compile schemas before pack

--- pack.sh
+++ pack.sh
@@ -2,7 +2,8 @@
 
 ZIP_NAME='./snowy@exposedcat.shell-extension.zip'
 
-# TODO: Build schemas
+rm -f "$ZIP_NAME" ./schemas/gschemas.compiled
 
-rm -f "$ZIP_NAME"
+glib-compile-schemas ./schemas
+
 zip -r "$ZIP_NAME" js schemas extension.js prefs.js stylesheet.css metadata.json
